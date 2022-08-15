41f1b28fb39d28031dcc2de42fb8d261f3dfa799
InariYuki <105775231+InariYuki@users.noreply.github.com>
2022-08-15 23:59:02 +0800

    finished

--- object pool and dots/Assets/BulletController.cs
+++ object pool and dots/Assets/BulletController.cs
@@ -1,6 +1,10 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.Mathematics;
+using Unity.Jobs;
+using Unity.Collections;
+using Unity.Burst;
 
 public class BulletController : MonoBehaviour
 {
@@ -14,12 +18,45 @@ public class BulletController : MonoBehaviour
     private void Update() {
         time += Time.deltaTime;
         if(time >= exist_time){
-            //Destroy(gameObject);
             ObjectPool<BulletController>.instance.Recycle(this);
         }
         transform.Translate(new Vector3(speed * Time.deltaTime * Mathf.Cos(rotation + time) , speed * Time.deltaTime * Mathf.Sin(rotation + time) , 0));
     }
     public void Reset(){
         time = 0;
+    }
+}
+public struct BulletData{
+    public float _speed , _rotation , _exist_time , _time , _deltaTime;
+    public bool _isReadyRecycle;
+    public float3 _position;
+    public BulletData(BulletController bc , float deltaTime){
+        _speed = bc.speed;
+        _rotation = bc.rotation;
+        _exist_time = bc.exist_time;
+        _time = bc.time;
+        _deltaTime = deltaTime;
+        _isReadyRecycle = false;
+        _position = bc.transform.position;
+    }
+    public void Calculate(){
+        _time += _deltaTime;
+        if(_time >= _exist_time){
+            _isReadyRecycle = true;
+        }
+        _position += new float3(_speed * _deltaTime * Mathf.Sin(_rotation + _time * 5) , _speed * _deltaTime * Mathf.Cos(_rotation + _time * 10) , 0);
+        float val = 1;
+        for(int i = 0; i < 100000; i++){
+            val += Mathf.Exp(Mathf.Sqrt(val));
+        }
+    }
+}
+[BurstCompile]
+public struct BulletUpdateJob : IJobParallelFor{
+    public NativeArray<BulletData> bulletDataList;
+    public void Execute(int index){
+        BulletData data = bulletDataList[index];
+        data.Calculate();
+        bulletDataList[index] = data;
     }
 }
--- object pool and dots/Assets/PlayerControll.cs
+++ object pool and dots/Assets/PlayerControll.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.Collections;
+using Unity.Jobs;
 
 public class PlayerControll : MonoBehaviour
 {
     [SerializeField] float speed = 5f;
     [SerializeField] GameObject bullet;
     ObjectPool<BulletController> bullet_pool;
+    List<BulletController> bulletUpdateList = new List<BulletController>();
     private void Awake() {
         bullet_pool = ObjectPool<BulletController>.instance;
         bullet_pool.InitPool(bullet);
@@ -14,6 +17,7 @@ public class PlayerControll : MonoBehaviour
         for(int i = 0; i < warm_up_count; i++){
             BulletController t = bullet_pool.Spawn(Vector3.zero , Quaternion.identity);
             t.time = 10;
+            bulletUpdateList.Add(t);
         }
     }
     private void Update() {
@@ -22,11 +26,30 @@ public class PlayerControll : MonoBehaviour
             float d = 4f;
             float d_radian = 360/d * Mathf.PI / 180;
             for(int i = 0; i < d; i++){
-                //BulletController bullet_instanced = Instantiate(bullet , transform.position , Quaternion.identity).GetComponent<BulletController>();
                 BulletController bullet_instanced = bullet_pool.Spawn(transform.position , Quaternion.identity);
                 bullet_instanced.rotation = d_radian * i;
                 bullet_instanced.Reset();
+                bulletUpdateList.Add(bullet_instanced);
             }
+        }
+        if(bulletUpdateList.Count != 0){
+            NativeArray<BulletData> nativeBulletDataList = new NativeArray<BulletData>(bulletUpdateList.Count , Allocator.TempJob);
+            for(int i = 0; i < bulletUpdateList.Count; i++){
+                nativeBulletDataList[i] = new BulletData(bulletUpdateList[i] , Time.deltaTime);
+            }
+            BulletUpdateJob bulletUpdateJob = new BulletUpdateJob();
+            bulletUpdateJob.bulletDataList = nativeBulletDataList;
+            JobHandle handle = bulletUpdateJob.Schedule(bulletUpdateList.Count , 100);
+            handle.Complete();
+            for(int i = bulletUpdateList.Count - 1; i >= 0; i--){
+                bulletUpdateList[i].transform.position = nativeBulletDataList[i]._position;
+                bulletUpdateList[i].time = nativeBulletDataList[i]._time;
+                if(nativeBulletDataList[i]._isReadyRecycle){
+                    bullet_pool.Recycle(bulletUpdateList[i]);
+                    bulletUpdateList.RemoveAt(i);
+                }
+            }
+            nativeBulletDataList.Dispose();
         }
     }
     private void FixedUpdate() {
